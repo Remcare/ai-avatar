d83ca6e37757c66cb6e4c02c5e307b1ba63c162d
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-10-15 22:01:40 +0100

    feat: interactive avatar centre of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@
 
 import InteractiveAvatar from "@/components/InteractiveAvatar";
 export default function App() {
-
   return (
     <div className="w-screen h-screen flex flex-col">
-      <div className="w-[900px] flex flex-col items-start justify-start gap-5 mx-auto pt-4 pb-20">
+      <div className="w-[900px] flex flex-col items-center justify-center gap-5 mx-auto h-full">
         <div className="w-full">
           <InteractiveAvatar />
         </div>
